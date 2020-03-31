46753054b8255f2f2d07acaaf99b7c6c8196d0ea
Guilxw <61891261+Guilxw@users.noreply.github.com>
2020-03-30 21:13:58 -0300

    Add files via upload

---
+++ atividade 3 POO/atividade 3 POO/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace atividade_3_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* menu*/
+            Console.WriteLine("Aluno : Guilherme Pereira \n");
+            Console.WriteLine("Selecione a opção desejada \n");
+            Console.WriteLine(" 1: Carimbar ");
+            Console.WriteLine(" 2: Escrever ");
+            Console.Write(" Digite a opção desejada: ");
+            /*recebe referencia*/
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            /* limpa a tela*/
+            
+            switch (opcao)
+            {
+                case 1:
+                    
+                    Console.WriteLine("Aluno : Guilherme Pereira\n ");
+                    Console.WriteLine(" Opção selecionada ( Carimbar)\n ");
+                    Console.Write(" Digite o texto desejado: ");
+                    string Ptexto = Console.ReadLine();
+                    carimbo x = new carimbo(Ptexto);
+                    /* Atribuir valores e referencias nos atributos*/
+                    Console.WriteLine("\n Carimbado!\n ");
+                    x.carregar(" Azul", 10);
+                    x.usar();
+                    Console.WriteLine(" Deseja Alterar texto e cor? 1 alterar / 2 sair \n");
+                    int selecao = Convert.ToInt32(Console.ReadLine());
+
+                    switch (selecao)
+                    {
+                        case 1:
+
+                            Console.Write("\n Digite o texto:");
+                            string Gtexto = Console.ReadLine();
+                            carimbo alteratexto = new carimbo(Gtexto);
+                            x.trocaTexto(Gtexto);
+                            x.trocarcor(" amarelo");
+                            x.usar();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            break;
+                    }
+                    
+                    Console.ReadKey();
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Aluno : Guilherme Pereira\n");
+                    Console.WriteLine("Opção selecionada ( Escrever)\n ");
+                    Console.Write(" Digite o texto desejado: \n");
+                    string Ptexto2 = Console.ReadLine();
+                    caneta y = new caneta(Ptexto2);
+                    Console.WriteLine("\n Escrito!");
+                    y.carregar(" Vermelho", 10);
+                    y.usar();
+                    Console.WriteLine(" Deseja Alterar texto e cor? 1 alterar / 2 sair \n");
+                    int selecao2 = Convert.ToInt32(Console.ReadLine());
+
+                    switch (selecao2)
+                    {
+                        case 1:
+
+                            Console.Write("\n Digite o texto:");
+                            string atexto = Console.ReadLine();
+                            carimbo alteratexto = new carimbo(atexto);
+                            y.trocaTexto(atexto);
+                            y.trocarcor(" amarelo");
+                            y.usar();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            break;
+                    }
+                    Console.ReadKey();
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ atividade 3 POO/atividade 3 POO/carimbo.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace atividade_3_POO
+{
+    class carimbo : Interacao
+    {
+        /*Atributos*/
+        private string cor;
+        private string texto;
+        private int carga;
+
+        /*contrutor*/
+        public carimbo(string Ptexto)
+        {
+            trocaTexto(Ptexto);
+
+        }
+
+        /* Metodos Get Set*/
+
+        private string Cor { 
+        get {return cor;}
+        set {cor = value; }}
+
+        private string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        private int Carga
+        {
+            get { return carga; }
+            set { carga = value; }
+        }
+
+        public void usar()
+        {
+            if (carga >= 1)
+                {if (!string.IsNullOrEmpty(texto))
+                {
+                    if (!string.IsNullOrEmpty(cor))
+                    {
+                        Console.WriteLine("(" + Texto + ") Carimbado na cor " + cor+ "\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossivel Carimbar - Carimbo está sem cor.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("impossivel carimbar - carimbo sem texto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("impossivel carimbar - Carimbo sem carga");
+            }
+        }
+
+        public void carregar(string Pcor, int Pcarga)
+        {
+            trocarcor(Pcor);
+
+
+            if ((Pcarga < 1 && Pcarga > 10) && ((Carga + Pcarga) > 10))
+            {
+                Console.WriteLine("carga Atual: " + carga + ", A carga deve ser entre 1 e 10");
+             }
+
+            else
+            {
+                Carga = Pcarga;
+            }
+        }
+
+        public void trocarcor (string Pcor)
+        {
+            if (Pcor == Cor)
+            {
+                Console.WriteLine("Carimbo já está com está cor!");
+            }
+            else
+            {
+                Cor = Pcor;
+            }
+        }
+
+        public void trocaTexto (string Ptexto)
+        {
+            if (Ptexto == Texto)
+            {
+                Console.WriteLine("carimbo já está com está cor!");
+            }
+
+            else
+            {
+                Texto =Ptexto;
+            }
+        }
+
+    }
+
+}
